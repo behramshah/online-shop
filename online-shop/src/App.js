36e284ff5c85a88e7398c338baf7ebfe6e0816af
Bahram <baxxxrrram@yandex.ru>
2022-05-20 18:05:18 +0400

    finishing with firebase authentication

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,14 +1,14 @@
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/signIn.component";
-import { Routes, Route } from "react-router-dom";
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component'
+import { Routes, Route } from 'react-router-dom';
 
 const  App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation/>}>
         <Route index path='/' element={<Home/>}/>
-        <Route index path='/sign-in' element={<SignIn/>}/>
+        <Route index path='/auth' element={<Authentication/>}/>
       </Route>
     </Routes>  
   );

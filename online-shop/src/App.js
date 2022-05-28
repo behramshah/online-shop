211fcc753f1f87e015ed7f20ffd921d807e17a4f
Bahram <baxxxrrram@yandex.ru>
2022-05-28 15:24:02 +0400

    shopping icon cart context and cart drop down

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,11 +1,8 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component'
-import { Routes, Route } from 'react-router-dom';
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from './routes/shop/shop.component';
 
 const  App = () => {
   return (
--- online-shop/src/contexts/user.context.jsx
+++ online-shop/src/contexts/user.context.jsx
@@ -8,7 +8,7 @@ export const UserContext = createContext({
 
 export const UserProvider = ({ children }) => {
 
-    const [currentUser, setCurrentUser]=useState(null);
+    const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
     
 
